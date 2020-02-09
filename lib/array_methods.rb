e6983a563256f739983e372aae7c4645a81089c7
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 21:16:10 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
+# This method should return the index of the value that was passed in. If the value is not found, this method should return nil.
+# To implement this method, you will need loop over the provided array and compare each value in the array to the value provided. Remember though, that this is not the value that needs to be returned
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+    array.index(value_to_find)
 end
 
+# This methods should return whichever integer in the array has the highest value
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
