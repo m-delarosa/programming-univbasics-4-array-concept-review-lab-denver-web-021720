9015ff7843b0e0a04cf137b78509ef8fd42f0eb1
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 20:59:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 # This method should return the index of the value that was passed in. If the value is not found, this method should return nil.
 # To implement this method, you will need loop over the provided array and compare each value in the array to the value provided. Remember though, that this is not the value that needs to be returned
 
-def find_element_index(array, value_to_find) do
+def find_element_index(array, value_to_find)
   array.length.times { | index |
     array.index("value_to_find")
   }
