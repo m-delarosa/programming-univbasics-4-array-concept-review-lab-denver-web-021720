3701819991d3a432b3afae4e47ba4c502f3f60a3
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-09 20:52:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,3 +1,4 @@
+#This method should return the index of the value that was passed in. If the value is not found, this method should return nil.
 def find_element_index(array, value_to_find)
   # Add your solution here
 end
